c9a1b75c22166e029361dd0bb7125b280c3f63ba
fred31YT <116939487+fred31YT@users.noreply.github.com>
2024-02-05 19:38:59 +0400

    Add files via upload

---
+++ Geography86/carta.js
@@ -0,0 +1,10 @@
+$('.part').hover (
+	function() {
+		$('.description').html($(this).attr('description-data'));
+		$('.description').fadeIn();
+	},
+	function() {
+		$('.description').fadeOut(50);
+	}
+)
+
